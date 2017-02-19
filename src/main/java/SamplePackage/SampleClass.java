1e00369cadef491cb301401d089aec9def2c6bc1
RameshYannam <rameshyannam@gmail.com>
2017-02-19 14:23:27 +0530

    New Intelli Project

---
+++ src/main/java/SamplePackage/SampleClass.java
@@ -0,0 +1,12 @@
+package SamplePackage;
+
+/**
+ * Created by LENOVO on 2/19/2017.
+ */
+public class SampleClass {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Ramesh Yannam");
+    }
+}
